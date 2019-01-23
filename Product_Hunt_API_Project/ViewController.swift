b9024a9b842238ce63d82ae5ce62231560a97adb
Jackson Ho <mintri1199@gmail.com>
2019-01-22 22:41:59 -0800

    Initial commit

---
+++ Product_Hunt_API_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Product_Hunt_API_Project
+//
+//  Created by Jackson Ho on 1/22/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
