dc2fd826a5ebd8f3b1f31bf3739196ef21d0f6b4
Jackson Ho <mintri1199@gmail.com>
2019-01-22 23:10:16 -0800

    Created Post model

---
+++ Product_Hunt_API_Project/models/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Product_Hunt_API_Project
+//
+//  Created by Jackson Ho on 1/22/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: String
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
